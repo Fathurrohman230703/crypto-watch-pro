49d944653b73965e59c491cb4d34a9282d3ca5c0
M Fathurrohman <abdillahfathurrohman5@gmail.com>
2026-07-04 10:00:48 +0700

    Setup app theme and go router

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 
+import 'router/app_router.dart';
+import 'theme/app_theme.dart';
+
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Crypto Watch Pro',
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      home: const Scaffold(
-        body: Center(
-          child: Text('Crypto Watch Pro'),
-        ),
-      ),
+      title: 'Crypto Watch Pro',
+      theme: AppTheme.lightTheme,
+      routerConfig: appRouter,
     );
   }
 }
---
+++ lib/app/router/app_router.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+final GoRouter appRouter = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const HomePage(),
+    ),
+  ],
+);
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Crypto Watch Pro'),
+      ),
+    );
+  }
+}

9fb57ddf573300647ecbd46e403bb7d1c0f2acb8
M Fathurrohman <abdillahfathurrohman5@gmail.com>
2026-07-04 10:08:21 +0700

    Setup dependency injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
 import 'app/app.dart';
+import 'injection/injection.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await initDependencies();
+
   runApp(const App());
 }

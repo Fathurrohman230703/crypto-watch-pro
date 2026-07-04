458a57a3aec9f6937c13d92464154cad041a09b9
M Fathurrohman <abdillahfathurrohman5@gmail.com>
2026-07-04 10:13:24 +0700

    Setup Dio client

--- lib/injection/injection.dart
+++ lib/injection/injection.dart
@@ -1,7 +1,12 @@
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+
+import '../core/network/dio_client.dart';
 
 final sl = GetIt.instance;
 
 Future<void> initDependencies() async {
-  // Register dependency di sini
+  sl.registerLazySingleton<Dio>(
+    () => DioClient.dio,
+  );
 }
